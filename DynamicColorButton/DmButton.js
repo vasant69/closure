c1fde48a5b2758076461c23c836ab525ed028f75
Vasant vyas <85029484+vasant69@users.noreply.github.com>
2025-03-15 18:08:24 +0530

    Dynamic Button Creation based on input color

--- DynamicColorButton/DmButton.js
+++ DynamicColorButton/DmButton.js
@@ -1,3 +1,19 @@
-AddBtnColor(){
-    console.log("AddBtnColor");
+function AddBtnColor(color){
+    debugger;
+    document.getElementById("createBtn").addEventListener("click", function(){
+        let color = document.getElementById("txt").value;
+
+        let btn = document.createElement("button");
+        console.log(btn)
+        btn.onclick = function(){
+            window.document.body.style.backgroundColor = color;
+            alert("Color Changed");
+        };
+
+        btn.innerHTML = color;
+        btn.style.backgroundColor = color;
+        btn.style.color = "white";
+        document.getElementById("buttonContainer").appendChild(btn);
+        btn = null;
+    });
 }
